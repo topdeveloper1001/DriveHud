eba4d52f0d4ea1614e6fdcf3d0856138d9aeedfd
Arthur Gritsenko <gricenko.a.v@gmail.com>
2016-10-11 22:18:45 -0500

    Changed hud right click behavior

--- DriveHUD.Application/Controls/HudPanel.xaml.cs
+++ DriveHUD.Application/Controls/HudPanel.xaml.cs
@@ -20,9 +20,44 @@ namespace DriveHUD.Application.Controls
     /// </summary>
     public partial class HudPanel : UserControl
     {
+        private System.Windows.Point _startPoint;
+        private bool _dragged = false;
+
         public HudPanel()
         {
             InitializeComponent();
+            this.PreviewMouseMove += HudRichPanel_PreviewMouseMove;
+            this.PreviewMouseRightButtonUp += HudRichPanel_PreviewMouseRightButtonUp;
+            this.AddHandler(UserControl.PreviewMouseRightButtonDownEvent, new MouseButtonEventHandler(HudRichPanel_PreviewMouseRightButtonDown), true);
+        }
+
+        private void HudRichPanel_PreviewMouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (_dragged)
+            {
+                e.Handled = true;
+            }
+            _dragged = false;
+        }
+
+        private void HudRichPanel_PreviewMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.RightButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                System.Windows.Point position = e.GetPosition(null);
+
+                if (Math.Abs(position.X - _startPoint.X) > System.Windows.SystemParameters.MinimumHorizontalDragDistance ||
+                        Math.Abs(position.Y - _startPoint.Y) > System.Windows.SystemParameters.MinimumVerticalDragDistance)
+                {
+                    _dragged = true;
+                }
+            }
+        }
+
+        private void HudRichPanel_PreviewMouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _startPoint = e.GetPosition(null);
+            _dragged = false;
         }
     }
 }
