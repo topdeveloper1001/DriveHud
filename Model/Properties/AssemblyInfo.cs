1d8b4da888a286662fa56c89bf362240fb3b5939
Alexander <aleksander.v.danilov@gmail.com>
2016-12-22 00:09:46 +0300

    fixed new install issues

--- Model/Properties/AssemblyInfo.cs
+++ Model/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.7.873")]
-[assembly: AssemblyVersion("1.0.7.873")]
-[assembly: AssemblyFileVersion("1.0.7.873")]
+// [assembly: AssemblyVersion("1.0.7.876")]
+[assembly: AssemblyVersion("1.0.7.876")]
+[assembly: AssemblyFileVersion("1.0.7.876")]
