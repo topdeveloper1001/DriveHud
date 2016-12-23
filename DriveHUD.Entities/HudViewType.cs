e0843cbc59034b79e1835ad9752ab36358cf38d9
Arthur Gritsenko <gricenko.a.v@gmail.com>
2016-12-22 19:48:20 -0600

    Added missing file

---
+++ DriveHUD.Entities/HudViewType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveHUD.Entities
+{
+    public enum HudViewType
+    {
+        Horizontal,
+        Vertical_1,
+        Vertical_2,
+        Plain
+    }
+}
