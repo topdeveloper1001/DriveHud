702f9779ea4c2685f9b48a9fcf627d6b78c54ad9
Arthur Gritsenko <gricenko.a.v@gmail.com>
2017-02-13 12:11:22 -0600

    Updated project version

--- DriveHUD.Application/Properties/AssemblyInfo.cs
+++ DriveHUD.Application/Properties/AssemblyInfo.cs
@@ -50,9 +50,9 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.8.1128")]
-[assembly: AssemblyVersion("1.0.8.1128")]
-[assembly: AssemblyFileVersion("1.0.8.1128")]
+// [assembly: AssemblyVersion("1.0.8.1134")]
+[assembly: AssemblyVersion("1.0.8.1134")]
+[assembly: AssemblyFileVersion("1.0.8.1134")]
 
 #if DEBUG
 
