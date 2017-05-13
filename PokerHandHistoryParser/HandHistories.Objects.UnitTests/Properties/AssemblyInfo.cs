c0a2559f92c06b015b485ab96181fa86c8197a3e
Alexander <aleksander.v.danilov@gmail.com>
2017-05-13 11:38:22 +0300

    Bumped Version to v.1.0.9.1287

--- PokerHandHistoryParser/HandHistories.Objects.UnitTests/Properties/AssemblyInfo.cs
+++ PokerHandHistoryParser/HandHistories.Objects.UnitTests/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.9.1285")]
-[assembly: AssemblyVersion("1.0.9.1285")]
-[assembly: AssemblyFileVersion("1.0.9.1285")]
+// [assembly: AssemblyVersion("1.0.9.1287")]
+[assembly: AssemblyVersion("1.0.9.1287")]
+[assembly: AssemblyFileVersion("1.0.9.1287")]
