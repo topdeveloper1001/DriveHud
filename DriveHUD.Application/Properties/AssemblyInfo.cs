54635b3d7c551c5577c4513a3be08de429bdc404
Alexander <aleksander.v.danilov@gmail.com>
2017-06-03 02:16:23 +0300

    Bumped version to 1.1.0.1405

--- DriveHUD.Application/Properties/AssemblyInfo.cs
+++ DriveHUD.Application/Properties/AssemblyInfo.cs
@@ -50,9 +50,9 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.0.1390")]
-[assembly: AssemblyVersion("1.1.0.1390")]
-[assembly: AssemblyFileVersion("1.1.0.1390")]
+// [assembly: AssemblyVersion("1.1.0.1405")]
+[assembly: AssemblyVersion("1.1.0.1405")]
+[assembly: AssemblyFileVersion("1.1.0.1405")]
 
 #if DEBUG
 
--- PokerHandHistoryParser/HandHistories.Objects/Properties/AssemblyInfo.cs
+++ PokerHandHistoryParser/HandHistories.Objects/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.0.1390")]
-[assembly: AssemblyVersion("1.1.0.1390")]
-[assembly: AssemblyFileVersion("1.1.0.1390")]
+// [assembly: AssemblyVersion("1.1.0.1405")]
+[assembly: AssemblyVersion("1.1.0.1405")]
+[assembly: AssemblyFileVersion("1.1.0.1405")]
 
 #if DEBUG
 
