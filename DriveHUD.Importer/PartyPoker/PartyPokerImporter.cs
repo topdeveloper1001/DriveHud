381cc494d8dd7f99b71027bc0b0c6883c5cf2d85
Arthur Gritsenko <gricenko.a.v@gmail.com>
2017-07-09 15:13:59 -0500

    turned on preferred seating

--- DriveHUD.Importer/PartyPoker/PartyPokerImporter.cs
+++ DriveHUD.Importer/PartyPoker/PartyPokerImporter.cs
@@ -4,7 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DriveHUD.Entities;
+using HandHistories.Objects.Hand;
+using HandHistories.Objects.Players;
 using HandHistories.Parser.Parsers;
+using HandHistories.Parser.Utils.FastParsing;
+using Model.Settings;
+using DriveHUD.Importers.Helpers;
+using Microsoft.Practices.ServiceLocation;
 
 namespace DriveHUD.Importers.PartyPoker
 {
@@ -54,6 +60,36 @@ namespace DriveHUD.Importers.PartyPoker
             }
 
             return title.Contains(parsingResult.Source.TableName);
+        }
+
+        protected override PlayerList GetPlayerList(HandHistory handHistory)
+        {
+            var playerList = handHistory.Players;
+
+            var maxPlayers = handHistory.GameDescription.SeatType.MaxPlayers;
+
+            var heroSeat = handHistory.Hero != null ? handHistory.Hero.SeatNumber : 0;
+
+            if (heroSeat != 0)
+            {
+                var preferredSeats = ServiceLocator.Current.GetInstance<ISettingsService>().GetSettings().
+                                        SiteSettings.SitesModelList.FirstOrDefault(x => x.PokerSite == EnumPokerSites.PartyPoker)?.PrefferedSeats;
+
+                var prefferedSeat = preferredSeats.FirstOrDefault(x => (int)x.TableType == maxPlayers && x.IsPreferredSeatEnabled);
+
+                if (prefferedSeat != null)
+                {
+                    var shift = (prefferedSeat.PreferredSeat - heroSeat) % maxPlayers;
+
+                    foreach (var player in playerList)
+                    {
+                        player.SeatNumber = GeneralHelpers.ShiftPlayerSeat(player.SeatNumber, shift, maxPlayers);
+                    }
+                }
+
+            }
+
+            return playerList;
         }
     }
 }
