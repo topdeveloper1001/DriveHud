9491b5e96f92d006de0b4b5af8d8f4e6b65bd6c8
Alexander <aleksander.v.danilov@gmail.com>
2017-07-14 16:28:41 +0300

    Bumped version to 1.1.0.1509

--- PokerHandHistoryParser/HandHistories.Parser/Properties/AssemblyInfo.cs
+++ PokerHandHistoryParser/HandHistories.Parser/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.0.1506")]
-[assembly: AssemblyVersion("1.1.0.1506")]
-[assembly: AssemblyFileVersion("1.1.0.1506")]
+// [assembly: AssemblyVersion("1.1.0.1509")]
+[assembly: AssemblyVersion("1.1.0.1509")]
+[assembly: AssemblyFileVersion("1.1.0.1509")]
 
 #if DEBUG
 
