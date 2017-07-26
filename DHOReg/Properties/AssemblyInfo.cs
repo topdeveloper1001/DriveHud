a62c145672ce3fc56764b17ef0e44bd8688893ae
Alexander <aleksander.v.danilov@gmail.com>
2017-07-26 20:33:51 +0300

    Fixed lic AssemblyInfo.cs

--- DHOReg/Properties/AssemblyInfo.cs
+++ DHOReg/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AcePokerSolutions LLC")]
 [assembly: AssemblyProduct("DHOReg")]
-[assembly: AssemblyCopyright("Copyright Ã‚Â©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
