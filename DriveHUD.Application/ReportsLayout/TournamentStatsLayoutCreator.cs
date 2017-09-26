15e40b4faa798c231c1fc0ce444853a5a87d2d3b
Alexander <aleksander.v.danilov@gmail.com>
2017-09-26 14:59:07 +0300

    Removed duplicates from columns

--- DriveHUD.Application/ReportsLayout/TournamentStatsLayoutCreator.cs
+++ DriveHUD.Application/ReportsLayout/TournamentStatsLayoutCreator.cs
@@ -30,27 +30,7 @@ namespace DriveHUD.Application.ReportsLayout
             gridView.Columns.Add(Add("Reports_Column_GameType", nameof(TournamentReportRecord.GameType)));
             gridView.Columns.Add(Add("Reports_Column_Speed", nameof(TournamentReportRecord.TournamentSpeed)));
             gridView.Columns.Add(AddFinancial("Reports_Column_Buyin", nameof(TournamentReportRecord.BuyIn)));
-
-            gridView.Columns.Add(AddPercentile("Reports_Column_VPIP", nameof(TournamentReportRecord.VPIP)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_PFR", nameof(TournamentReportRecord.PFR)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_AggPercent", nameof(TournamentReportRecord.AggPr)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_Agg", nameof(TournamentReportRecord.Agg)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_3Bet", nameof(TournamentReportRecord.ThreeBet)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_WTSD", nameof(TournamentReportRecord.WTSD)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_WSSD", nameof(TournamentReportRecord.WSSD)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_WSWSF", nameof(TournamentReportRecord.WSWSF)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_FlopToCBet", nameof(TournamentReportRecord.FlopCBet)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_FoldToFlopCBet", nameof(TournamentReportRecord.FoldCBet)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_Steal", nameof(TournamentReportRecord.Steal)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_BlindsReRaiseSteal", nameof(TournamentReportRecord.BlindsReraiseSteal)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_BlindsFoldToSteal", nameof(TournamentReportRecord.BlindsFoldSteal)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_4Bet", nameof(TournamentReportRecord.FourBet)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_UO_PFR_EP", nameof(TournamentReportRecord.UO_PFR_EP)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_UO_PFR_MP", nameof(TournamentReportRecord.UO_PFR_MP)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_UO_PFR_CO", nameof(TournamentReportRecord.UO_PFR_CO)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_UO_PFR_BTN", nameof(TournamentReportRecord.UO_PFR_BN)));
-            gridView.Columns.Add(AddPercentile("Reports_Column_UO_PFR_SB", nameof(TournamentReportRecord.UO_PFR_SB)));
-
+   
             AddDefaultStats(gridView, defaultColumns);
 
             foreach (var column in gridView.Columns)
