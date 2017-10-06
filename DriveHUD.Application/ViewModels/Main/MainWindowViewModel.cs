848a9e6e73d7851708463948146eace56f15eeea
Alexander <aleksander.v.danilov@gmail.com>
2017-10-07 00:14:11 +0300

    Bumper stickers for Hero uses session data instead of historical

--- DriveHUD.Application/ViewModels/Main/MainWindowViewModel.cs
+++ DriveHUD.Application/ViewModels/Main/MainWindowViewModel.cs
@@ -647,7 +647,8 @@ namespace DriveHUD.Application.ViewModels
                             Player = playerCollectionItem,
                             Layout = activeLayout.Name,
                             Statistic = lastHandStatistic,
-                            StickerFilters = stickers
+                            StickerFilters = stickers,
+                            IsHero = sessionCacheStatistic.IsHero
                         };
 
                         if (stickers.Count > 0)
