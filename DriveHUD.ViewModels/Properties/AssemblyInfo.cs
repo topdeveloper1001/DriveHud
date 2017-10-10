c50791f0edd903875695419fcd2e6849d0ff18d0
Alexander <aleksander.v.danilov@gmail.com>
2017-10-10 08:07:20 +0300

    Bumped version to 1.2.2.1695

--- DriveHUD.ViewModels/Properties/AssemblyInfo.cs
+++ DriveHUD.ViewModels/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.2.1693")]
-[assembly: AssemblyVersion("1.2.2.1693")]
-[assembly: AssemblyFileVersion("1.2.2.1693")]
+// [assembly: AssemblyVersion("1.2.2.1695")]
+[assembly: AssemblyVersion("1.2.2.1695")]
+[assembly: AssemblyFileVersion("1.2.2.1695")]
