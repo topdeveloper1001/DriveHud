e586f8ea8a273424174e02f7d4afad6e7fe7fd79
Alexander <aleksander.v.danilov@gmail.com>
2017-10-18 08:17:09 +0300

    Bumped version to 1.3.0.1724

--- DriveHUD.Updater/Properties/AssemblyInfo.cs
+++ DriveHUD.Updater/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.0.1718")]
-[assembly: AssemblyVersion("1.3.0.1718")]
-[assembly: AssemblyFileVersion("1.3.0.1718")]
+// [assembly: AssemblyVersion("1.3.0.1724")]
+[assembly: AssemblyVersion("1.3.0.1724")]
+[assembly: AssemblyFileVersion("1.3.0.1724")]
--- PokerHandHistoryParser/HandHistories.Objects/Properties/AssemblyInfo.cs
+++ PokerHandHistoryParser/HandHistories.Objects/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.0.1718")]
-[assembly: AssemblyVersion("1.3.0.1718")]
-[assembly: AssemblyFileVersion("1.3.0.1718")]
+// [assembly: AssemblyVersion("1.3.0.1724")]
+[assembly: AssemblyVersion("1.3.0.1724")]
+[assembly: AssemblyFileVersion("1.3.0.1724")]
 
 #if DEBUG
 
