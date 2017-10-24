9e339133c37fb7704fd3587de449e92124f692f2
Alexander <aleksander.v.danilov@gmail.com>
2017-10-24 20:28:25 +0300

    DHUD-316 Bumped version to 1.3.0.1735

--- PokerHandHistoryParser/HandHistories.Objects/Properties/AssemblyInfo.cs
+++ PokerHandHistoryParser/HandHistories.Objects/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.0.1733")]
-[assembly: AssemblyVersion("1.3.0.1733")]
-[assembly: AssemblyFileVersion("1.3.0.1733")]
+// [assembly: AssemblyVersion("1.3.0.1735")]
+[assembly: AssemblyVersion("1.3.0.1735")]
+[assembly: AssemblyFileVersion("1.3.0.1735")]
 
 #if DEBUG
 
