d91b93c9169c51e5133f9f711084fb7506f3633b
Alexander <aleksander.v.danilov@gmail.com>
2017-11-10 11:38:31 +0300

    Bumped version to 1.3.0.1760

--- DriveHUD.Bootstrapper.App/Properties/AssemblyInfo.cs
+++ DriveHUD.Bootstrapper.App/Properties/AssemblyInfo.cs
@@ -52,8 +52,8 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.0.1758")]
-[assembly: AssemblyVersion("1.3.0.1758")]
-[assembly: AssemblyFileVersion("1.3.0.1758")]
+// [assembly: AssemblyVersion("1.3.0.1760")]
+[assembly: AssemblyVersion("1.3.0.1760")]
+[assembly: AssemblyFileVersion("1.3.0.1760")]
 
 [assembly: BootstrapperApplication(typeof(BootstrapperApp))]
