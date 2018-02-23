c1bbf3688c20b15637070377778f5f6449c2e141
Alexander <aleksander.v.danilov@gmail.com>
2018-02-23 20:04:34 +0300

    Fixed auto-seats for PM

--- DriveHUD.Importer/ExternalImporter/ExternalImporter.cs
+++ DriveHUD.Importer/ExternalImporter/ExternalImporter.cs
@@ -221,7 +221,7 @@ namespace DriveHUD.Importers.ExternalImporter
                 case EnumPokerSites.PokerMaster:
                     return new Dictionary<int, int>
                     {
-                        { 4, 3 },
+                        { 2, 2 },
                         { 3, 2 },
                         { 4, 3 },
                         { 5, 3 },
