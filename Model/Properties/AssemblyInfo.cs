4f9ad986ad6686806f584e25bc8ade63be8c2c60
Alexander <aleksander.v.danilov@gmail.com>
2018-03-15 13:36:36 +0300

    Bumped version to 1.4.4.2085

--- Model/Properties/AssemblyInfo.cs
+++ Model/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.4.2081")]
-[assembly: AssemblyVersion("1.4.4.2081")]
-[assembly: AssemblyFileVersion("1.4.4.2081")]
+// [assembly: AssemblyVersion("1.4.4.2085")]
+[assembly: AssemblyVersion("1.4.4.2085")]
+[assembly: AssemblyFileVersion("1.4.4.2085")]
