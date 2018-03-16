a5f72d1ea84b1da52f37d0ea002f70558237e4cc
Alexander <aleksander.v.danilov@gmail.com>
2018-03-16 08:45:17 +0300

    Removed OK button from filter form, form will be closed on apply button

--- DriveHUD.Application/Views/PopupContainers/PopupContainerFiltersView.xaml.cs
+++ DriveHUD.Application/Views/PopupContainers/PopupContainerFiltersView.xaml.cs
@@ -1,21 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DriveHUD.Application.ViewModels.PopupContainers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-using Telerik.Windows.Controls;
-
-using DriveHUD.Application.ViewModels.PopupContainers;
 
 namespace DriveHUD.Application.Views.PopupContainers
 {
@@ -28,17 +13,7 @@ namespace DriveHUD.Application.Views.PopupContainers
         {
             InitializeComponent();
         }
-
-        private void Apply_Click(object sender, RoutedEventArgs e)
-        {
-            (DataContext as PopupContainerBaseFilterViewModel).Apply_CommandClick.Execute(null);
-        }
-
-        private void OK_Click(object sender, RoutedEventArgs e)
-        {
-            (DataContext as PopupContainerBaseFilterViewModel).Ok_CommandClick.Execute(null);
-        }
-
+    
         private void UserControl_Unloaded(object sender, RoutedEventArgs e)
         {
             (DataContext as PopupContainerBaseFilterViewModel).RestoreDefaultFiltersState_Command.Execute(null);
