4decfdb2ad81db79e8c02c173338dee49a84e253
Alexander <aleksander.v.danilov@gmail.com>
2018-03-29 23:03:43 +0300

    Bumped version to 1.4.4.2116

--- DriveHUD.HUD.Service/Properties/AssemblyInfo.cs
+++ DriveHUD.HUD.Service/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.4.2110")]
-[assembly: AssemblyVersion("1.4.4.2110")]
-[assembly: AssemblyFileVersion("1.4.4.2110")]
+// [assembly: AssemblyVersion("1.4.4.2116")]
+[assembly: AssemblyVersion("1.4.4.2116")]
+[assembly: AssemblyFileVersion("1.4.4.2116")]
