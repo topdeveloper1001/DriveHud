d37fad30b82e036a4720bd4621b8393c5e370fe5
Alexander <aleksander.v.danilov@gmail.com>
2018-04-03 00:06:06 +0300

    Bumped version to 1.4.4.2127

--- DriveHUD.Application/Properties/AssemblyInfo.cs
+++ DriveHUD.Application/Properties/AssemblyInfo.cs
@@ -50,9 +50,9 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.4.2122")]
-[assembly: AssemblyVersion("1.4.4.2122")]
-[assembly: AssemblyFileVersion("1.4.4.2122")]
+// [assembly: AssemblyVersion("1.4.4.2127")]
+[assembly: AssemblyVersion("1.4.4.2127")]
+[assembly: AssemblyFileVersion("1.4.4.2127")]
 
 #if DEBUG
 
--- DriveHUD.Importer/Properties/AssemblyInfo.cs
+++ DriveHUD.Importer/Properties/AssemblyInfo.cs
@@ -38,9 +38,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.4.2122")]
-[assembly: AssemblyVersion("1.4.4.2122")]
-[assembly: AssemblyFileVersion("1.4.4.2122")]
+// [assembly: AssemblyVersion("1.4.4.2127")]
+[assembly: AssemblyVersion("1.4.4.2127")]
+[assembly: AssemblyFileVersion("1.4.4.2127")]
 
 #if DEBUG
 
