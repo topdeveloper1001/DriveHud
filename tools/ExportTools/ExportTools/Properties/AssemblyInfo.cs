04c94bc57614e9ab28aa1092f56429f7f9570bf7
Alexander <aleksander.v.danilov@gmail.com>
2018-04-16 20:41:12 +0300

    Bumped version to 1.4.4.2143

--- tools/ExportTools/ExportTools/Properties/AssemblyInfo.cs
+++ tools/ExportTools/ExportTools/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.4.2135")]
-[assembly: AssemblyVersion("1.4.4.2135")]
-[assembly: AssemblyFileVersion("1.4.4.2135")]
+// [assembly: AssemblyVersion("1.4.4.2143")]
+[assembly: AssemblyVersion("1.4.4.2143")]
+[assembly: AssemblyFileVersion("1.4.4.2143")]
