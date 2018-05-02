89cf7b24dc5acce004456cff3d4d26bc669083ae
Alexander <aleksander.v.danilov@gmail.com>
2018-05-02 18:11:58 +0300

    Added Ignition emulator

---
+++ Simulator/IgnitionEmulator.cs
@@ -0,0 +1,18 @@
+﻿using DriveHUD.Entities;
+
+namespace Simulator
+{
+    public class IgnitionEmulator : PokerStarsEmulator
+    {
+        public IgnitionEmulator()
+        {
+            HandHistoryLocation = @"d:\Git\DriveHUD\Simulator\TestData\Ignition";
+        }
+
+        public override string Name => "Ignition Emulator";
+
+        public override EnumPokerSites Site => EnumPokerSites.Ignition;
+
+        protected override string HandHistoryFilter => "*.xml";
+    }
+}
