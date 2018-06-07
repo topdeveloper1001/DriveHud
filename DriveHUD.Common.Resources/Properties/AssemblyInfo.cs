d84cc069424349dcb49aa69bf9c58a6f8d1b824c
Alexander <aleksander.v.danilov@gmail.com>
2018-06-08 00:28:18 +0300

    Bumped version to 1.4.5.2247

--- DriveHUD.Common.Resources/Properties/AssemblyInfo.cs
+++ DriveHUD.Common.Resources/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.5.2243")]
-[assembly: AssemblyVersion("1.4.5.2243")]
-[assembly: AssemblyFileVersion("1.4.5.2243")]
+// [assembly: AssemblyVersion("1.4.5.2247")]
+[assembly: AssemblyVersion("1.4.5.2247")]
+[assembly: AssemblyFileVersion("1.4.5.2247")]
--- PokerHandHistoryParser/HandHistories.Objects/Properties/AssemblyInfo.cs
+++ PokerHandHistoryParser/HandHistories.Objects/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.5.2243")]
-[assembly: AssemblyVersion("1.4.5.2243")]
-[assembly: AssemblyFileVersion("1.4.5.2243")]
+// [assembly: AssemblyVersion("1.4.5.2247")]
+[assembly: AssemblyVersion("1.4.5.2247")]
+[assembly: AssemblyFileVersion("1.4.5.2247")]
 
 #if DEBUG
 
