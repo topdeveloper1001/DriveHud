5f3f70fa69e274a0419a90a5e7b4c727eca448ea
Alexander <aleksander.v.danilov@gmail.com>
2018-08-02 02:02:51 +0300

    Bumped version to 1.5.0.2307

--- Model/Properties/AssemblyInfo.cs
+++ Model/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.0.2303")]
-[assembly: AssemblyVersion("1.5.0.2303")]
-[assembly: AssemblyFileVersion("1.5.0.2303")]
+// [assembly: AssemblyVersion("1.5.0.2307")]
+[assembly: AssemblyVersion("1.5.0.2307")]
+[assembly: AssemblyFileVersion("1.5.0.2307")]
