9040d39c6625a9dea56f206f9a11afd7e87aab54
Alexander <aleksander.v.danilov@gmail.com>
2018-08-29 23:16:16 +0300

    DHUD-405 Added test for PKHandBuilder

--- DriveHud.Tests/TcpImportersTests/PKTests/PKHandBuilderTests.cs
+++ DriveHud.Tests/TcpImportersTests/PKTests/PKHandBuilderTests.cs
@@ -10,12 +10,17 @@
 // </copyright>
 //----------------------------------------------------------------------
 
+using DriveHUD.Common.Extensions;
+using DriveHUD.Importers.PokerKing;
+using DriveHUD.Importers.PokerKing.Model;
+using HandHistories.Objects.Hand;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using NUnit.Framework;
+using ProtoBuf;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace DriveHud.Tests.TcpImportersTests.PKTests
 {
@@ -23,13 +28,193 @@ namespace DriveHud.Tests.TcpImportersTests.PKTests
     class PKHandBuilderTests
     {
         private const string TestDataFolder = "TcpImportersTests\\PKTests\\TestData\\HandsRawData";
-        private const string JsonExt = ".json";
+        private const string SourceJsonFile = "Source.json";
         private const string ExpectedResultFile = "Result.xml";
 
-        [TestCase("")]
-        public void TryBuildTest()
+        [OneTimeSetUp]
+        public void SetUp()
         {
+            Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
+        }
 
+        [TestCase("8-max-regular-no-hero", 105772)]
+        public void TryBuildTest(string testFolder, long heroId)
+        {
+            var packages = ReadPackages(testFolder);
+
+            CollectionAssert.IsNotEmpty(packages, $"Packages collection must be not empty for {testFolder}");
+
+            var handBuilder = new PKHandBuilder();
+
+            HandHistory actual = null;
+
+            foreach (var package in packages)
+            {
+                if (handBuilder.TryBuild(package, out actual))
+                {
+                    break;
+                }
+            }
+
+            Assert.IsNotNull(actual, $"Actual HandHistory must be not null for {testFolder}");
+
+            var expected = ReadExpectedHandHistory(testFolder);
+
+            Assert.IsNotNull(expected, $"Expected HandHistory must be not null for {testFolder}");
+
+            AssertionUtils.AssertHandHistory(actual, expected);
+        }
+
+        private HandHistory ReadExpectedHandHistory(string folder)
+        {
+            var xmlFile = Path.Combine(TestDataFolder, folder, ExpectedResultFile);
+
+            FileAssert.Exists(xmlFile);
+
+            try
+            {
+                var handHistoryText = File.ReadAllText(xmlFile);
+                return SerializationHelper.DeserializeObject<HandHistory>(handHistoryText);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail($"{ExpectedResultFile} in {folder} has not been deserialized: {e}");
+            }
+
+            return null;
+        }
+
+        private IEnumerable<PokerKingPackage> ReadPackages(string folder)
+        {
+            var packages = new List<PokerKingPackage>();
+
+            var jsonFile = Path.Combine(TestDataFolder, folder, SourceJsonFile);
+
+            FileAssert.Exists(jsonFile);
+
+            PKTestSourceObject testSourceObject = null;
+
+            try
+            {
+                var jsonFileContent = File.ReadAllText(jsonFile);
+                testSourceObject = JsonConvert.DeserializeObject<PKTestSourceObject>(jsonFileContent, new StringEnumConverter());
+            }
+            catch (Exception e)
+            {
+                Assert.Fail($"{ExpectedResultFile} in {folder} has not been deserialized: {e}");
+            }
+
+            Assert.IsNotNull(testSourceObject);
+
+            foreach (var packet in testSourceObject.Packets)
+            {
+                PokerKingPackage package = null;
+
+                switch (packet.PackageType)
+                {
+                    case PackageType.RequestLeaveRoom:
+                        package = BuildPackage<RequestLeaveRoom>(packet);
+                        break;
+                    case PackageType.NoticeStartGame:
+                        package = BuildPackage<NoticeStartGame>(packet);
+                        break;
+                    case PackageType.NoticeResetGame:
+                        package = BuildPackage<NoticeResetGame>(packet);
+                        break;
+                    case PackageType.NoticeGamePost:
+                        package = BuildPackage<NoticeGamePost>(packet);
+                        break;
+                    case PackageType.NoticeGameAnte:
+                        package = BuildPackage<NoticeGameAnte>(packet);
+                        break;
+                    case PackageType.NoticeGameElectDealer:
+                        package = BuildPackage<NoticeGameElectDealer>(packet);
+                        break;
+                    case PackageType.NoticeGameBlind:
+                        package = BuildPackage<NoticeGameBlind>(packet);
+                        break;
+                    case PackageType.NoticeGameHoleCard:
+                        package = BuildPackage<NoticeGameHoleCard>(packet);
+                        break;
+                    case PackageType.NoticePlayerAction:
+                        package = BuildPackage<NoticePlayerAction>(packet);
+                        break;
+                    case PackageType.NoticeGameRoundEnd:
+                        package = BuildPackage<NoticeGameRoundEnd>(packet);
+                        break;
+                    case PackageType.NoticeGameCommunityCards:
+                        package = BuildPackage<NoticeGameCommunityCards>(packet);
+                        break;
+                    case PackageType.NoticeGameShowCard:
+                        package = BuildPackage<NoticeGameShowCard>(packet);
+                        break;
+                    case PackageType.NoticeGameSettlement:
+                        package = BuildPackage<NoticeGameSettlement>(packet);
+                        break;
+                    case PackageType.NoticeGameShowDown:
+                        package = BuildPackage<NoticeGameShowDown>(packet);
+                        break;
+                    case PackageType.NoticePlayerStayPosition:
+                        package = BuildPackage<NoticePlayerStayPosition>(packet);
+                        break;
+                    case PackageType.NoticePlayerShowCard:
+                        package = BuildPackage<NoticePlayerShowCard>(packet);
+                        break;
+                    case PackageType.NoticeBuyin:
+                        package = BuildPackage<NoticeBuyin>(packet);
+                        break;
+                    case PackageType.NoticeGameSnapShot:
+                        package = BuildPackage<NoticeGameSnapShot>(packet);
+                        break;
+                    case PackageType.RequestHeartBeat:
+                        package = BuildPackage<RequestHeartBeat>(packet);
+                        break;
+                    default:
+                        Assert.Fail($"Unsupported package type: {packet.PackageType}");
+                        break;
+                }
+
+                Assert.IsNotNull(package);
+
+                packages.Add(package);
+            }
+
+            return packages;
+        }
+
+        private PokerKingPackage BuildPackage<T>(PKTestSourcePacket packet)
+        {
+            var contentObject = JsonConvert.DeserializeObject<T>(packet.Content.ToString(), new StringEnumConverter());
+
+            Assert.IsNotNull(contentObject, $"Content object of {typeof(T)} must be not null.");
+
+            using (var ms = new MemoryStream())
+            {
+                Serializer.Serialize(ms, contentObject);
+                var bytes = ms.ToArray();
+
+                var package = new PokerKingPackage
+                {
+                    PackageType = packet.PackageType,
+                    Body = bytes
+                };
+
+                return package;
+            }
+        }
+
+        private class PKTestSourceObject
+        {
+            public IEnumerable<PKTestSourcePacket> Packets { get; set; }
+        }
+
+        private class PKTestSourcePacket
+        {
+            public PackageType PackageType { get; set; }
+
+            public DateTime Time { get; set; }
+
+            public object Content { get; set; }
         }
     }
 }
