6951884b33113eac82556588ddde0671f81c09f6
Alexander <aleksander.v.danilov@gmail.com>
2018-09-04 20:56:16 +0300

    Bumped version to 1.5.1.2384

--- DriveHUD.Importer/Properties/AssemblyInfo.cs
+++ DriveHUD.Importer/Properties/AssemblyInfo.cs
@@ -38,9 +38,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.1.2382")]
-[assembly: AssemblyVersion("1.5.1.2382")]
-[assembly: AssemblyFileVersion("1.5.1.2382")]
+// [assembly: AssemblyVersion("1.5.1.2384")]
+[assembly: AssemblyVersion("1.5.1.2384")]
+[assembly: AssemblyFileVersion("1.5.1.2384")]
 
 #if DEBUG
 
