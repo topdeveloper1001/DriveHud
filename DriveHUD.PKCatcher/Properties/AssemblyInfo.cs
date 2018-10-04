48a483be757737aa32ce47790fceb247bcfe23c1
Alexander <aleksander.v.danilov@gmail.com>
2018-10-04 16:57:52 +0300

    Bumped version to 1.5.2.2447

--- DriveHUD.PKCatcher/Properties/AssemblyInfo.cs
+++ DriveHUD.PKCatcher/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.94")]
-[assembly: AssemblyVersion("1.0.0.94")]
-[assembly: AssemblyFileVersion("1.0.0.94")]
+// [assembly: AssemblyVersion("1.0.0.97")]
+[assembly: AssemblyVersion("1.0.0.97")]
+[assembly: AssemblyFileVersion("1.0.0.97")]
