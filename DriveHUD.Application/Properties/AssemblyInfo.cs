0348a81729c759031de8b4c7569c35c8c69105c8
Alexander <aleksander.v.danilov@gmail.com>
2018-11-16 02:37:54 +0300

    Bumped version to 1.5.3.2484

--- DriveHUD.Application/Properties/AssemblyInfo.cs
+++ DriveHUD.Application/Properties/AssemblyInfo.cs
@@ -50,9 +50,9 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.3.2482")]
-[assembly: AssemblyVersion("1.5.3.2482")]
-[assembly: AssemblyFileVersion("1.5.3.2482")]
+// [assembly: AssemblyVersion("1.5.3.2484")]
+[assembly: AssemblyVersion("1.5.3.2484")]
+[assembly: AssemblyFileVersion("1.5.3.2484")]
 
 #if DEBUG
 
