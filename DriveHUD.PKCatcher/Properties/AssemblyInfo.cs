12ea8d83f40caf17c3efd9f0101b9322443d882d
Alexander <aleksander.v.danilov@gmail.com>
2018-12-06 09:58:24 +0300

    Bumped version to 1.6.0.2572

--- DriveHUD.PKCatcher/Properties/AssemblyInfo.cs
+++ DriveHUD.PKCatcher/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.214")]
-[assembly: AssemblyVersion("1.0.0.214")]
-[assembly: AssemblyFileVersion("1.0.0.214")]
+// [assembly: AssemblyVersion("1.0.0.222")]
+[assembly: AssemblyVersion("1.0.0.222")]
+[assembly: AssemblyFileVersion("1.0.0.222")]
--- PokerHandHistoryParser/HandHistories.Parser/Properties/AssemblyInfo.cs
+++ PokerHandHistoryParser/HandHistories.Parser/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.0.2564")]
-[assembly: AssemblyVersion("1.6.0.2564")]
-[assembly: AssemblyFileVersion("1.6.0.2564")]
+// [assembly: AssemblyVersion("1.6.0.2572")]
+[assembly: AssemblyVersion("1.6.0.2572")]
+[assembly: AssemblyFileVersion("1.6.0.2572")]
 
 #if DEBUG
 
