8c11a5ba0a008463988c3b1942977ee16e7edfb7
Alexander <aleksander.v.danilov@gmail.com>
2018-12-10 23:44:45 +0300

    Bumped version to 1.6.0.2581

--- DriveHUD.EquityCalculator/Properties/AssemblyInfo.cs
+++ DriveHUD.EquityCalculator/Properties/AssemblyInfo.cs
@@ -50,9 +50,9 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.0.2577")]
-[assembly: AssemblyVersion("1.6.0.2577")]
-[assembly: AssemblyFileVersion("1.6.0.2577")]
+// [assembly: AssemblyVersion("1.6.0.2581")]
+[assembly: AssemblyVersion("1.6.0.2581")]
+[assembly: AssemblyFileVersion("1.6.0.2581")]
 
 #if DEBUG
 
--- DriveHUD.HUD/Properties/AssemblyInfo.cs
+++ DriveHUD.HUD/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.0.2577")]
-[assembly: AssemblyVersion("1.6.0.2577")]
-[assembly: AssemblyFileVersion("1.6.0.2577")]
+// [assembly: AssemblyVersion("1.6.0.2581")]
+[assembly: AssemblyVersion("1.6.0.2581")]
+[assembly: AssemblyFileVersion("1.6.0.2581")]
