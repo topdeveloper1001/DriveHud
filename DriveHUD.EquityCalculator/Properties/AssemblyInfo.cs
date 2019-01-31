d90be1ab437bdb1111c3fbba1668e9ae6975b88f
Alexander <aleksander.v.danilov@gmail.com>
2019-01-31 23:55:01 +0300

    Bumped version to 1.6.1.2692

--- DriveHUD.EquityCalculator/Properties/AssemblyInfo.cs
+++ DriveHUD.EquityCalculator/Properties/AssemblyInfo.cs
@@ -50,9 +50,9 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.1.2685")]
-[assembly: AssemblyVersion("1.6.1.2685")]
-[assembly: AssemblyFileVersion("1.6.1.2685")]
+// [assembly: AssemblyVersion("1.6.1.2692")]
+[assembly: AssemblyVersion("1.6.1.2692")]
+[assembly: AssemblyFileVersion("1.6.1.2692")]
 
 #if DEBUG
 
--- PokerHandHistoryParser/HandHistories.Parser/Properties/AssemblyInfo.cs
+++ PokerHandHistoryParser/HandHistories.Parser/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.1.2685")]
-[assembly: AssemblyVersion("1.6.1.2685")]
-[assembly: AssemblyFileVersion("1.6.1.2685")]
+// [assembly: AssemblyVersion("1.6.1.2692")]
+[assembly: AssemblyVersion("1.6.1.2692")]
+[assembly: AssemblyFileVersion("1.6.1.2692")]
 
 #if DEBUG
 
