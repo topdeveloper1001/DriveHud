4118f481587ddb35e46cf1975a3558d01f389474
Alexander <aleksander.v.danilov@gmail.com>
2019-02-04 14:32:13 +0300

    Bumped version to 1.6.1.2698

--- DriveHUD.Bootstrapper.App/Properties/AssemblyInfo.cs
+++ DriveHUD.Bootstrapper.App/Properties/AssemblyInfo.cs
@@ -52,8 +52,8 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.1.2695")]
-[assembly: AssemblyVersion("1.6.1.2695")]
-[assembly: AssemblyFileVersion("1.6.1.2695")]
+// [assembly: AssemblyVersion("1.6.1.2698")]
+[assembly: AssemblyVersion("1.6.1.2698")]
+[assembly: AssemblyFileVersion("1.6.1.2698")]
 
 [assembly: BootstrapperApplication(typeof(BootstrapperApp))]
--- PokerHandHistoryParser/HandHistories.Parser/Properties/AssemblyInfo.cs
+++ PokerHandHistoryParser/HandHistories.Parser/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.1.2695")]
-[assembly: AssemblyVersion("1.6.1.2695")]
-[assembly: AssemblyFileVersion("1.6.1.2695")]
+// [assembly: AssemblyVersion("1.6.1.2698")]
+[assembly: AssemblyVersion("1.6.1.2698")]
+[assembly: AssemblyFileVersion("1.6.1.2698")]
 
 #if DEBUG
 
