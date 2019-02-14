eae5d5278eb9a6cba554b793cba4191a791c554b
Alexander <aleksander.v.danilov@gmail.com>
2019-02-14 18:15:07 +0300

    Bumped version to 1.6.1.2734

--- PokerHandHistoryParser/HandHistories.Parser.WindowsTestApp/Properties/AssemblyInfo.cs
+++ PokerHandHistoryParser/HandHistories.Parser.WindowsTestApp/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.1.2729")]
-[assembly: AssemblyVersion("1.6.1.2729")]
-[assembly: AssemblyFileVersion("1.6.1.2729")]
+// [assembly: AssemblyVersion("1.6.1.2734")]
+[assembly: AssemblyVersion("1.6.1.2734")]
+[assembly: AssemblyFileVersion("1.6.1.2734")]
