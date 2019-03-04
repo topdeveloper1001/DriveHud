5d8b518506c0abf0bd611ed2184261113edc723f
Alexander <aleksander.v.danilov@gmail.com>
2019-03-05 01:26:31 +0300

    Bumped version to 1.6.1.2758

--- DHOReg/Properties/AssemblyInfo.cs
+++ DHOReg/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.1.2754")]
-[assembly: AssemblyVersion("1.6.1.2754")]
-[assembly: AssemblyFileVersion("1.6.1.2754")]
+// [assembly: AssemblyVersion("1.6.1.2758")]
+[assembly: AssemblyVersion("1.6.1.2758")]
+[assembly: AssemblyFileVersion("1.6.1.2758")]
--- DriveHUD.Bootstrapper.App/Properties/AssemblyInfo.cs
+++ DriveHUD.Bootstrapper.App/Properties/AssemblyInfo.cs
@@ -52,8 +52,8 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.1.2754")]
-[assembly: AssemblyVersion("1.6.1.2754")]
-[assembly: AssemblyFileVersion("1.6.1.2754")]
+// [assembly: AssemblyVersion("1.6.1.2758")]
+[assembly: AssemblyVersion("1.6.1.2758")]
+[assembly: AssemblyFileVersion("1.6.1.2758")]
 
 [assembly: BootstrapperApplication(typeof(BootstrapperApp))]
