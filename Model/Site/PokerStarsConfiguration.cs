b7393b4a0b051449f2d667531023bc0e898e1cd1
Alexander <aleksander.v.danilov@gmail.com>
2019-03-08 16:59:13 +0300

    Fix issue when DH shows warning about language for PS.IN

--- Model/Site/PokerStarsConfiguration.cs
+++ Model/Site/PokerStarsConfiguration.cs
@@ -43,7 +43,7 @@ namespace Model.Site
         private const string iniSaveMyHandsPath = "SaveMyHandsPath";
         private const string iniSaveMyTournSummariesPath = "SaveMyTournSummariesPath";
         private const string settingsFileName = "user.ini";
-        private const string correctLanguageSetting = "0";
+        private static readonly string[] correctLanguageSettings = new[] { "0", "33" };
         private const string psClientLaunchFile = "PokerStars.exe";
         private const string displayNameKey = "DisplayName";
         private const string displayNameKeyValuePattern = "PokerStars";
@@ -216,10 +216,10 @@ namespace Model.Site
 
                     LogProvider.Log.Info($"PS Settings: Locale: {localeSetting}; HHLocale: {hhLocaleSetting}; TSLocale: {tsLocaleSetting}");
 
-                    var isLocaleSettingCorrect = localeSetting == correctLanguageSetting;
+                    var isLocaleSettingCorrect = correctLanguageSettings.Contains(localeSetting);
 
-                    bool isHHLanguageCorrect = string.IsNullOrWhiteSpace(hhLocaleSetting) ? isLocaleSettingCorrect : hhLocaleSetting == correctLanguageSetting;
-                    bool isTSLanguageCorrect = string.IsNullOrWhiteSpace(tsLocaleSetting) ? isLocaleSettingCorrect : tsLocaleSetting == correctLanguageSetting;
+                    bool isHHLanguageCorrect = string.IsNullOrWhiteSpace(hhLocaleSetting) ? isLocaleSettingCorrect : correctLanguageSettings.Contains(hhLocaleSetting);
+                    bool isTSLanguageCorrect = string.IsNullOrWhiteSpace(tsLocaleSetting) ? isLocaleSettingCorrect : correctLanguageSettings.Contains(tsLocaleSetting);
                     bool isHHEnabled = hhEnabledSetting == "1";
                     bool isTSEnabled = tsEnabledSetting == "1";
 
