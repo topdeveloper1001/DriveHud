19b99b2da0c483395f44f181ff4dcccc9d94d509
Alexander <aleksander.v.danilov@gmail.com>
2019-03-27 19:00:15 +0300

    Bumped version to 1.6.1.2792

--- DriveHUD.Updater.Core/Properties/AssemblyInfo.cs
+++ DriveHUD.Updater.Core/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.6.1.2790")]
-[assembly: AssemblyVersion("1.6.1.2790")]
-[assembly: AssemblyFileVersion("1.6.1.2790")]
+// [assembly: AssemblyVersion("1.6.1.2792")]
+[assembly: AssemblyVersion("1.6.1.2792")]
+[assembly: AssemblyFileVersion("1.6.1.2792")]
